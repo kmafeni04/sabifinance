7a12be14bc162f48d37bb1100232d87cd7634ffe
kmafeni04 <komemafeni944@gmail.com>
2024-05-18 17:03:05 +0100

    test railway db

--- config.lua
+++ config.lua
@@ -2,17 +2,23 @@ local config = require("lapis.config")
 
 config({ "development", "production" }, {
   server = "nginx",
-  sqlite = {
-    database = "app.sqlite",
-  }
 })
 
 config("development", {
   code_cache = "off",
   num_workers = 1,
+  sqlite = {
+    database = "app.sqlite",
+  }
 })
 
 config("production", {
   code_cache = "on",
   -- num_workers = 4,
+  postgres = {
+    host = "viaduct.proxy.rlwy.net",
+    user = "postgres",
+    password = "qujyiitMZHNWRugSDUwJFAbxOMlhfyor",
+    database = "railway"
+  }
 })
